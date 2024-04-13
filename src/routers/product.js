fe9d272c3732e44006d4a60adc5a1ba1122bc4f8
MinaMaged2 <mena.maged949@gmail.com>
2024-04-13 17:09:30 +0200

    add edit product code

--- src/routers/product.js
+++ src/routers/product.js
@@ -79,7 +79,7 @@ router.put("/product/:id", async (req, res) => {
   const productID = req.params.id;
 
   const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "cost", "price", "online"];
+  const allowedUpdates = ["name", "cost", "price", "online", "code"];
   const isValidUpdates = updates.every((update) =>
     allowedUpdates.includes(update)
   );
